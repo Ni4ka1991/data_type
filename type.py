969cfcc01839119d63ff1ad95e33674c8f815e24
Ni4ka1991 <astahova_nica@mail.ru>
2021-03-08 16:19:14 +0200

    Add file type.ty

--- type.py
+++ type.py
@@ -1,9 +1,25 @@
-<<<<<<< HEAD
-X = 1000
-print(type ("Тип данных переменной X: ", X))
-=======
-x = 1000
-print(type ("Тип данных переменной x: ", x))
-print("bla - bla- bla")
-print("alb-alb-alb")
->>>>>>> 269842c7cc1e5a62cc9f1642ee7ca7bc04ff78e9
+#!/usr/bin/env python3
+
+#x = 1000
+#print("Class of variable x is: ", type (x))
+
+i = -10
+print("Class of variable i is: ", type (i))
+
+f = 0.001
+print("Class of variable f is: ", type (f))
+
+s = "abc"
+print("Class of variable s is: ", type (s))
+
+l = [1, 2, 3]
+print("Class of variable l is: ", type (l))
+
+d = {'a' : 1, 'b' : 2, 'c' : 3}  
+print("Class of variable d is: ", type (d))
+
+b = False
+print("Class of variable b is: ", type (b))
+
+x = f > i
+print("Class of variable x is: ", type (x))
